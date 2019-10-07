b612707f7c666b53977e1184e38e4fb28ab7d074
Tkachenino <54236969+Tkachenino@users.noreply.github.com>
2019-10-07 17:18:47 +0300

    минимизация

--- js/popup.js
+++ js/popup.js
@@ -22,7 +22,9 @@ if (storage1) {
 if (storage2) {
   child.value = storage2;
 }
-
+document.addEventListener("DOMContentLoaded", function () {
+  popup.classList.add("hidden");
+});
 switchp.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (!popup.classList.contains("hidden")) {
